173183000ec30d51633a47ab98c6cbfdd10ac8b0
Nicole <nyarbrough14@gmail.com>
2021-05-15 16:48:52 -0700

    Update

---
+++ test_palindrome.py
@@ -0,0 +1,10 @@
+import unittest
+import palindrome
+
+class testCaseAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.isPalindrome(2, 1), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
